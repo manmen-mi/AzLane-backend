1a3e8a38a4853c13fb88419f7e002beafddfc8fc
kim <manmen.mi1375@gmail.com>
2021-09-20 15:58:25 +0900

    deno app

---
+++ main.ts
@@ -0,0 +1,52 @@
+import { Application, Router, helpers } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+const router = new Router();
+router.get("/main/:chapter/:stage", async (ctx) => {
+    const params = helpers.getQuery(ctx, { mergeParams: true });
+    const file = await Deno.readTextFile('./chapters.json');
+    const source = JSON.parse(file);
+
+    const chapter = +params.chapter;
+    const stage = +params.stage;
+    if (!chapter || !stage) {
+      ctx.throw(404);
+    }
+
+    ctx.response.body = JSON.stringify(source[chapter - 1][stage].normal);
+  })
+  .get("/event/:key/:stage", async (ctx) => {
+    const params = helpers.getQuery(ctx, { mergeParams: true });
+
+    const evKey = params.key;
+    const file = await Deno.readTextFile(`./${evKey}.json`);
+    const source = JSON.parse(file);
+
+    const stage = +params.stage;
+    if (!stage) {
+      ctx.throw(404);
+    }
+
+    ctx.response.body = JSON.stringify(source[stage].normal);
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
